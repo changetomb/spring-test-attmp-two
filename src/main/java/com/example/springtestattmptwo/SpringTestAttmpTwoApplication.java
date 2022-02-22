c521b3d7cf38be5daff5161aca9573f2450eb6c9
acer <mo.bodagh@yahoo.com>
2022-02-22 07:43:30 -0800

    comiit

--- src/main/java/com/example/springtestattmptwo/SpringTestAttmpTwoApplication.java
+++ src/main/java/com/example/springtestattmptwo/SpringTestAttmpTwoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
@@ -15,7 +16,7 @@ public class SpringTestAttmpTwoApplication {
 	}
 
 
-	@RequestMapping("/")
+	@RequestMapping(value = "/",method = {RequestMethod.GET,RequestMethod.POST})
 	@ResponseBody
 	public String index()
 	{
